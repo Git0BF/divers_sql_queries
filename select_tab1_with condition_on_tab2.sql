4b036347897a3ad4043ee1a52e4694492d914b60
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-12-04 11:09:45 +0100

    Update select_tab1_with condition_on_tab2.sql

--- select_tab1_with condition_on_tab2.sql
+++ select_tab1_with condition_on_tab2.sql
@@ -25,4 +25,14 @@ ON table_1.id = table_2.id
 INNER JOIN table_3
 ON table_1.id = table_3.id
 WHERE condition IN (SELECT condition
-                   FROM table_4)                  
+                   FROM table_4)    
+
+
+--Or you can select rows in one table based on condition on multiple tables
+
+SELECT *
+FROM table_1
+WHERE condition_1 IN (SELECT condition_1
+                      FROM table_2
+                      WHERE condition_2 IN (SELECT condition_2
+                                            FROM table_3))
