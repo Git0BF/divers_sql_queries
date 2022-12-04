68535562a565e44a891dc7483a809adee7d4c2c9
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-12-04 11:03:10 +0100

    Update select_tab1_with condition_on_tab2.sql

--- select_tab1_with condition_on_tab2.sql
+++ select_tab1_with condition_on_tab2.sql
@@ -2,3 +2,27 @@ SELECT *
 FROM table_1
 WHERE condition IN (SELECT condition
                    FROM table_2)
+                   
+                   
+--With rows selected from multiple tables with condition from one table
+
+SELECT *
+FROM table_1
+INNER JOIN table_2
+ON table_1.id = table_2.id
+INNER JOIN table_3
+ON table_1.id = table_3.id
+WHERE condition IN (SELECT condition
+                   FROM table_4)
+                   
+--Or you can write it in a new table :
+
+CREATE TABLE new_table AS
+SELECT *
+FROM table_1
+INNER JOIN table_2
+ON table_1.id = table_2.id
+INNER JOIN table_3
+ON table_1.id = table_3.id
+WHERE condition IN (SELECT condition
+                   FROM table_4)                  
