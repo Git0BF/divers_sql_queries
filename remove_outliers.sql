347899d4882b07344a02f7d6c87a14343e7c6190
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-12-04 11:20:21 +0100

    Update remove_outliers.sql

--- remove_outliers.sql
+++ remove_outliers.sql
@@ -5,3 +5,11 @@ WHERE column < (SELECT AVG(column) - 3 * STDEV(column)
                 FROM table)
 OR column > (SELECT AVG(column) + 3 * STDEV(column)
              FROM table)
+             
+-- Interquartile range, keep all the rows between 25% and 75% on a numerical column
+
+DELETE FROM table
+WHERE column < (SELECT PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY column)
+                FROM table)
+OR column > (SELECT PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY column)
+             FROM table)
